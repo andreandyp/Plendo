c6c112081d709129a68d9d1d799b852920cf1755
André Michel Andy Pozos <andreandyp@outlook.com>
2017-06-27 19:20:01 -0500

    Ya funciona la conexión a la API, falta ordenar y actualizar

--- public/js/plendo.js
+++ public/js/plendo.js
@@ -1,9 +1,27 @@
-$(document).ready( (jq) => {
-    const rutas
-    const vm = new Vue({
-        el: "#aplicacion",
-        data:{
-            nombre: "Andy"
-        }
+$(document).ready((jq) => {
+
+    const router = new VueRouter({
+        routes: [
+            { path: "/", component: {
+                template: "<div> <p v-if='cargando'>Cargando...</p> <div class='col-sm-4 col-sm-offset-4' v-for='queja in quejas' ><h3>(@{{queja.autor}}) dice:</h3><p>{{queja.texto}}</p><small>A las {{queja.fechaHora}}</small></div></div>",
+                data: function(){
+                    return {quejas: [], cargando: false}
+                },
+                beforeCreate: function(){
+                    this.cargando = true;
+                },
+                created: function(){
+                    this.$http.get('/api/quejas',).then( (response) =>{
+                        this.quejas = response.body;
+                    }, () => { console.log("Error en resource"); } );
+                }              
+            } }
+            //{ path: "/autentificar", component: autentificar }
+        ]
     });
+
+    const app = new Vue({
+        el: "#plendo",
+        router: router
+    })
 });

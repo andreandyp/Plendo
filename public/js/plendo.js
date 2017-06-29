b3e90fec12db9f475e62c3742aa08184a471f7fa
André Michel Andy Pozos <andreandyp@outlook.com>
2017-06-28 21:10:11 -0500

    Ya funciona el enrutamiento. Putos archivos .vue

--- public/js/plendo.js
+++ public/js/plendo.js
@@ -2,21 +2,57 @@ $(document).ready((jq) => {
 
     const router = new VueRouter({
         routes: [
-            { path: "/", component: {
-                template: "<div> <p v-if='cargando'>Cargando...</p> <div class='col-sm-4 col-sm-offset-4' v-for='queja in quejas' ><h3>(@{{queja.autor}}) dice:</h3><p>{{queja.texto}}</p><small>A las {{queja.fechaHora}}</small></div></div>",
-                data: function(){
-                    return {quejas: [], cargando: false}
-                },
-                beforeCreate: function(){
-                    this.cargando = true;
-                },
-                created: function(){
-                    this.$http.get('/api/quejas',).then( (response) =>{
-                        this.quejas = response.body;
-                    }, () => { console.log("Error en resource"); } );
-                }              
-            } }
-            //{ path: "/autentificar", component: autentificar }
+            {
+                path: "/", component: {
+                    template: "#api",
+                    data: function () {
+                        return { quejas: [], cargando: false }
+                    },
+                    filters: {
+                        mostrarFecha: (fecha) => {
+                            fecha = new Date(fecha);
+                            var fechaHora = [];
+                            fechaHora.push(fecha.getDate());
+                            fechaHora.push("/");
+                            fechaHora.push(fecha.getMonth() + 1);
+                            fechaHora.push("/");
+                            fechaHora.push(fecha.getFullYear());
+                            fechaHora.push(" a las ");
+                            fechaHora.push(fecha.getHours());
+                            fechaHora.push(":");
+                            fechaHora.push(fecha.getMinutes());
+                            return fechaHora.join("");
+                        }
+                    },
+                    beforeCreate: function () {
+                        this.cargando = true;
+                    },
+                    created: function () {
+                        this.$http.get('/api/quejas', ).then((response) => {
+                            this.cargando = false;
+                            this.quejas = response.body.reverse();
+                        }, () => { console.log("Error en resource"); });
+                    }
+                }
+            },
+            {
+                path: "/autentificar", component: {
+                    template: "#autentificar",
+                    data: function () {
+                        return {
+                            datos: {
+                                usuario: "",
+                                contraseña: ""
+                            },
+                            nuevo: {
+                                nombre: "",
+                                usuario: "",
+                                contraseña: "",
+                            }
+                        }
+                    }
+                }
+            }
         ]
     });
 
